0df57a292782a902b4739ca533a56ebbbde792ce
doumbia-alt <ousmanedoumby@gmail.com>
2022-06-29 15:26:25 +0000

    whitenoise

--- disnad/prod_settings.py
+++ disnad/prod_settings.py
@@ -4,6 +4,9 @@ from disnad.settings import*
 DEBUG = False
 TEMPLATE_DEBUG = False
 
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASES['default'] = dj_database_url.config()
 SECRET_KEY = '91q@vd01_b2zym!w^@(mc7%8yq^2zw3c#$mp(_&u!e@lg!^$(4'
 ALLOWED_HOSTS = ['isnad.herokuapp.com']
