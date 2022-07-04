ba9fc7b59c6963111b0d28342fcd14833497b95a
doumbia-alt <ousmanedoumby@gmail.com>
2022-07-04 21:27:11 +0000

    add templatefonctio

--- apps/models.py
+++ apps/models.py
@@ -9,7 +9,7 @@ class Contact(models.Model):
  prenom = models.TextField(null=True , blank=True)
  email = models.EmailField(null=True , blank=True)
  sujet = models.TextField()
- message = models.TextField(null=True , blank=True)
+ message = models.TextField()
  date = models.DateTimeField(default=timezone.now)
  #likes = models.IntegerField(default=0)
  def __str__(self):
@@ -19,7 +19,7 @@ class Pre_inscription(models.Model):
  nom = models.TextField()
  prenom = models.TextField()
  email = models.EmailField(null=True , blank=True)
- telephone = models.IntegerField(default=0)
+ telephone = models.IntegerField(default=1, null=True , blank=True)
  ecole = models.TextField()
  formation = models.TextField()
  filiere = models.TextField()
