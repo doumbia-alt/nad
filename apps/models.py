0a5f076831003132c2edde64e38c1597fa44e904
doumbia-alt <ousmanedoumby@gmail.com>
2022-07-04 11:55:20 +0000

    add model

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Contact(models.Model):
+ nom = models.TextField()
+ prenom = models.TextField()
+ email = models.EmailField(null=True , blank=True)
+ sujet = models.TextField()
+ message = models.TextField(null=True , blank=True)
+ date = models.DateTimeField(default=timezone.now)
+ #likes = models.IntegerField(default=0)
+ def __str__(self):
+	 return self.nom
+
+class Pre_inscription(models.Model):
+ nom = models.TextField()
+ prenom = models.TextField()
+ email = models.EmailField(null=True , blank=True)
+ sujet = models.TextField()
+ ecole = models.TextField()
+ formation = models.TextField()
+ filiere = models.TextField()
+ date = models.DateTimeField(default=timezone.now)
+ #likes = models.IntegerField(default=0)
+    
+ def __str__(self):
+	 return self.nom
--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.utils import timezone
+from .models import *
+from django.core.paginator import Paginator
 # Create your views here.
 
 def index(request):
@@ -27,9 +29,34 @@ def apropos(request):
     return render(request, 'a_propos.html', {})
 
 def contact(request):
+    
+    if request.method == "POST":
+        #context = {'success': False}
+        fistname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        print(fistname,lastname,email,subject,message)
+        voir = Contact(nom = fistname, prenom = lastname, email = email , sujet = subject , message = message)
+        voir.save()
     return render(request, 'contact.html', {})
 
 def preinscription(request):
+    
+    if request.method == "POST":
+        #context = {'success': False}
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        schoolId = request.POST['schoolId']
+        trainingId = request.POST['trainingId']
+        facultyId = request.POST['facultyId']
+        
+        print(firstname,lastname,email,phone,schoolId,trainingId,facultyId)
+        voir = Pre_inscription(nom = firstname, prenom = lastname, email = email , telephone = phone , ecole = schoolId , formation = trainingId , filiere = facultyId)
+        voir.save()
     return render(request, 'pre_inscription.html', {})
 
 def rhcom(request):
