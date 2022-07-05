d0a4824b2a508bd2eb8dc2f86b60fc512f1419e4
doumbia-alt <ousmanedoumby@gmail.com>
2022-07-05 19:32:11 +0000

    initial commit

--- apps/models.py
+++ apps/models.py
@@ -14,19 +14,6 @@ class Contact(models.Model):
  def __str__(self):
 	 return self.nom
 
-class Pre_inscription(models.Model):
- nom = models.TextField()
- prenom = models.TextField()
- email = models.EmailField(null=True , blank=True)
- phone = models.CharField(max_length=12)
- ecole = models.TextField()
- formation = models.TextField()
- filiere = models.TextField()
- date = models.DateTimeField(default=timezone.now)
- #likes = models.IntegerField(default=0)
-    
- def __str__(self):
-	 return self.nom
 
 class Cre_inscription(models.Model):
  nom = models.TextField()
