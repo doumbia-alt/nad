f47cfaa1562f91bb5c7ca07732f36a760dea8a57
doumbia-alt <ousmanedoumby@gmail.com>
2022-07-05 20:44:56 +0000

    initial commit

--- apps/models.py
+++ apps/models.py
@@ -3,16 +3,16 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Contact(models.Model):
- nom = models.TextField()
- prenom = models.TextField(null=True , blank=True)
- email = models.EmailField(null=True , blank=True)
- sujet = models.TextField()
- message = models.TextField()
- date = models.DateTimeField(default=timezone.now)
+#class Contact(models.Model):
+ #nom = models.TextField()
+ #prenom = models.TextField(null=True , blank=True)
+ #email = models.EmailField(null=True , blank=True)
+ #sujet = models.TextField()
+ #message = models.TextField()
+ #date = models.DateTimeField(default=timezone.now)
  #likes = models.IntegerField(default=0)
- def __str__(self):
-	 return self.nom
+ #def __str__(self):
+#	 return self.nom
 
 
 class Cre_inscription(models.Model):
@@ -26,5 +26,5 @@ class Cre_inscription(models.Model):
  date = models.DateTimeField(default=timezone.now)
  #likes = models.IntegerField(default=0)
     
- def __str__(self):
+def __str__(self):
 	 return self.nom
