bd036a58acc90e949e72ed860f3b70b9e3b15310
doumbia-alt <ousmanedoumby@gmail.com>
2022-07-12 15:54:06 +0000

    google analitic

--- disnad/settings.py
+++ disnad/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
-import dj_database_url
-from psycopg2 import DatabaseError
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -78,7 +77,12 @@ WSGI_APPLICATION = 'disnad.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DatabaseError['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'da',
+    }
+}
 
 
 
