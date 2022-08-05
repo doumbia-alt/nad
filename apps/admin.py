4e5ec6f6702b32c1a10d3437a638c239504023a6
doumbia-alt <ousmanedoumby@gmail.com>
2022-08-05 22:16:00 +0000

    google analitic

--- apps/admin.py
+++ apps/admin.py
@@ -3,6 +3,5 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Contact)
-@admin.site.register(Cre_inscription)
-class Creinscription(admin.ModelAdmin)
+@admin.site.register(Cre_inscription):
 list_display = ('nom','prenom','email','phone','ecole','formation','filiere','date')
